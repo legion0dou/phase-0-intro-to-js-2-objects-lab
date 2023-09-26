f167314d727d8b86cea2c842abe24b2a45620f0d
user_name <email_id>
2023-09-26 11:07:52 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution in this file!
+// Define an employee Object with name and streetAddress keys.
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+  };
+  
+  // Function to update an employee's key with a new value (non-destructive).
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  // Function to update an employee's key with a new value (destructive).
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete a key from an employee Object (non-destructive).
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to delete a key from an employee Object (destructive).
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  // Testing the functions
+  console.log(employee);
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Alice");
+  console.log(updatedEmployee);
+  
+  const destructiveUpdate = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "456 Elm St");
+  console.log(destructiveUpdate);
+  
+  const deletedEmployee = deleteFromEmployeeByKey(employee, "name");
+  console.log(deletedEmployee);
+  
+  const destructiveDelete = destructivelyDeleteFromEmployeeByKey(employee, "streetAddress");
+  console.log(destructiveDelete);
+  
